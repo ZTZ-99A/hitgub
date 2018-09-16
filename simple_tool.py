cc635643265c215725ca6fc598fbc3a7321fa919
ZTZ-99A <42900822+ZTZ-99A@users.noreply.github.com>
2018-09-16 11:38:00 +1000

    Update simple_tool.py

--- simple_tool.py
+++ simple_tool.py
@@ -37,4 +37,45 @@ def find_distance(a, b, c, d):
 def arithmeti_sequence(a, b):
     return int(((a + b) * (b - a + 1)) / 2)
 
+#determine all numbers of a list is greater than a certain number
+#a is a list
+def greater_certain_number(a, b):
+    ans = True
+    for i in a:
+        if i <= b:
+            ans = False
+            break
+    return ans
+
+#Find common elements in a list
+def common_elements(a, b):
+    list_a = set(a)
+    list_b = set(b)
+    common = [ ]
+    for i in list_a:
+        if i in list_b:
+            common.append(i)
+    if len(common) == 0:
+        return False
+    else:
+        return common
+
+#Create a Fibonnaci list
+def fibonnachi_list(n):
+    fibonnachi = [1,1]
+    a = 0
+    if n == 1:
+        return [1]
+    if n == 2:
+        return [1,1]
+    if n > 2:
+        while a < n - 2:
+            i = fibonnachi[a]
+            o = fibonnachi[a+1]
+            s = i + o
+            fibonnachi.append(s)
+            a = a + 1
+        while a == n - 2:
+            return fibonnachi
+
             
